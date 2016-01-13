51dfb5fe8ce10e103531f8f7940dee2159e6634b
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-01-13 22:53:03 +0200

    Add a makefile for .cpp compilation and used namespaces

--- opencv_test.cpp
+++ opencv_test.cpp
@@ -1,17 +1,20 @@
 
 /*
-$ g++ -lopencv_highgui test.cpp -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_stitching
-$ ./a.out
+$ g++ -lopencv_highgui opencv_test.cpp -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_stitching -o opencv_test
+$ ./opencv_test
 
 */
 #include <iostream>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
+using namespace std;
+using namespace cv;
+
 int main() {
-  std::cout << "Hello world\n";
+  cout << "Hello world\n";
   
-  cv::Mat img = cv::imread("camelia-logo.png", CV_LOAD_IMAGE_UNCHANGED);
+  Mat img = imread("camelia-logo.png", CV_LOAD_IMAGE_UNCHANGED);
   imshow("Foo", img);
-  cv::waitKey(0);
+  waitKey(0);
 }
