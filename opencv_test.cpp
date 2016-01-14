6a67dc3ef3efe3d0c744aafb371b78bb80157276
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-01-14 14:46:42 +0200

    Shared library C for wrapping OpenCV C++ API

--- opencv_test.cpp
+++ opencv_test.cpp
@@ -1,20 +1,28 @@
 
-/*
-$ g++ -lopencv_highgui opencv_test.cpp -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_stitching -o opencv_test
-$ ./opencv_test
+#include <stdio.h>
 
-*/
-#include <iostream>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
-using namespace cv;
 
-int main() {
-  cout << "Hello world\n";
-  
-  Mat img = imread("camelia-logo.png", CV_LOAD_IMAGE_UNCHANGED);
-  imshow("Foo", img);
-  waitKey(0);
+extern "C" {
+
+  void* imread(char *filename, int flags ) {
+    //void* mat = cv::imread(filename, flags);
+    //return &mat;
+    return NULL;
+  }
+
+  void imshow(char *winname, void* mat) {
+    printf("imshow!\n");
+    fflush(stdout);
+    cv::imshow(winname, (cv::InputArray)mat);
+  }
+
+  void namedWindow(char *winname, int flags ) {
+    printf("namedWindow\n");
+    fflush(stdout);
+    cv::namedWindow(winname, flags);
+  }
 }
+
