068416c02c55f94f7a80f31c586ba3b8161d6be1
azawawi <ahmad.zawawi@gmail.com>
2018-08-15 03:41:20 +0300

    Add mime-type/language detection and a couple of TODOs

--- ruby-tika.rb
+++ ruby-tika.rb
@@ -14,25 +14,75 @@ class TikaWrapper
     @port = port
   end
 
+  def rmeta
+    # TODO: rmeta
+  end
+
+  def unpack
+    # TODO: unpack
+  end
+
+  def parsers
+    # TODO: parsers
+  end
+
+  def detectors
+    # TODO: detectors
+  end
+
   def version
     uri = URI("http://#{@hostname}:#{@port}/version")
     Net::HTTP.get(uri)
   end
 
-  def meta(filename)
-    content_type = MIME::Types.type_for(filename).first.content_type
-    res = RestClient::Resource.new("http://#{@hostname}:#{@port}/meta")
+  def meta(filename, content_type = nil)
+    content_type ||= MIME::Types.type_for(filename).first.content_type
+    res = RestClient::Resource.new(_url('meta'))
     res.put File.read(filename), content_type: content_type
   end
 
-  def text(filename)
-    content_type = MIME::Types.type_for(filename).first.content_type
-    res = RestClient::Resource.new("http://#{@hostname}:#{@port}/tika")
+  def text(filename, content_type = nil)
+    content_type ||= MIME::Types.type_for(filename).first.content_type
+    res = RestClient::Resource.new(_url('tika'))
     res.put File.read(filename), content_type: content_type
+  end
+
+  def mime_type(filename)
+    res = RestClient::Resource.new(_url('detect/stream'))
+    res.put File.read(filename),
+            'Content-Disposition' => "attachment; filename=#{filename}"
+  end
+
+  def language(string)
+    res = RestClient::Resource.new(_url('language/string'))
+    res.put string
+  end
+
+  def _url(endpoint)
+    "http://#{@hostname}:#{@port}/#{endpoint}"
+  end
+
+  def _truncate(string, length)
+    if string.length <= length
+      string
+    else
+      string[0, string.rindex(/\s/, length - 3)].rstrip + '...'
+    end
   end
 end
 
 t = TikaWrapper.new
-puts t.meta('phocapdf-demo.pdf')
-puts t.meta('demo.docx')
-puts t.version
+puts "Found #{t.version} server"
+['demo.docx', 'a.docx'].each do |filename|
+  content_type = t.mime_type(filename)
+  puts "Detected stream type #{content_type}"
+
+  metadata = t.meta(filename, content_type)
+  puts "Metadata for #{filename}:\n#{t._truncate(metadata, 40)}"
+
+  text = t.text(filename)
+  puts "Found #{text.length} plain text"
+
+  language = t.language(text)
+  puts "Detected language #{language}"
+end
