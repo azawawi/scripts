fe68b06f0f89667cd413d224464a1c73bd2849c4
azawawi <ahmad.zawawi@gmail.com>
2018-11-21 19:51:14 +0200

    Simplify code

--- test-ffi.pl
+++ test-ffi.pl
@@ -14,9 +14,7 @@ record_layout(
 #--> main
 package main;
 
-use v5.16;
-
-use IO::Handle;
+use Modern::Perl '2018';
 use FFI::Platypus;
 
 STDOUT->autoflush(1);
@@ -52,7 +50,7 @@ $ffi->attach(
 $ffi->attach( clang_visitChildren =>
       [ 'CXCursor', 'CXCursorVisitor', 'CXClientData' ] => 'uint' );
 
-say clang_getClangVersion;
+say clang_getClangVersion();
 my $index = clang_createIndex( 0, 0 );
 say "index = $index";
 die "Error in clang_createIndex" unless $index;
@@ -90,8 +88,6 @@ my $visitChildren = $ffi->closure(
 );
 clang_visitChildren( $cursor, $visitChildren, 0 );
 
-END {
-    say "Destroying...";
-    clang_disposeTranslationUnit($translation_unit) if $translation_unit;
-    clang_disposeIndex($index)                      if $index;
-}
+say "Destroying...";
+clang_disposeTranslationUnit($translation_unit) if $translation_unit;
+clang_disposeIndex($index)                      if $index;
